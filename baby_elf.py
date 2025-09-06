ed94aa0ad9775ccf90d8b4cd155f6ef85c9ce717
aidenm0627 <aiden13miller@gmail.com>
2025-09-05 22:28:20 -0500

    Added and Updated Baby elf enemy

--- baby_elf.py
+++ baby_elf.py
@@ -3,4 +3,4 @@ from enemy import Enemy
 class BabyElf(Enemy):
     def take_damage(self, damage):
         self.health -= damage
-        print("Cry!! how could you hit a baby 😭, YOU MONSTER")
+        print("Cry!! how could you hit a baby 😭, YOU MONSTER!")
